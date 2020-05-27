188255c34e3aac3eafb6829d30a1a385aa71deaa
David King <dxking16@gmail.com>
2020-05-27 15:24:20 -0700

    remove hardcoded vpcId

--- midterm.sh
+++ midterm.sh
@@ -3,15 +3,17 @@
 # grab my local public IP
 lPubIp="`curl -s https://checkip.amazonaws.com`/32"
 sshKey='aws'
-amiID='068663a3c619dd892' #ubuntu 20.04
+amiID='068663a3c619dd892' # ubuntu 20.04
 iCount='1'
 iType='t2.micro'
-iVpcId='vpc-64b8b41e' # this is my default aws vpc
+# grab default vpc Id
+iVpcId=`aws ec2 describe-vpcs --filters Name=isDefault,Values=true | jq --raw-output '.Vpcs[0].VpcId'`
 
 # create security group and find Id with jq
 iSgName="midterm-sg-`head /dev/urandom | tr -dc a-z0-9 | head -c 3`"
 iSg=`aws ec2 create-security-group --group-name $iSgName --description 'midterm security group' --vpc-id $iVpcId`
 iSgId=`echo $iSg | jq --raw-output '.GroupId'`
+
 # add custom rules to security group to allow ssh/http
 aws ec2 authorize-security-group-ingress --group-id $iSgId --protocol tcp --port 22 --cidr $lPubIp
 aws ec2 authorize-security-group-ingress --group-id $iSgId --protocol tcp --port 80 --cidr 0.0.0.0/0
