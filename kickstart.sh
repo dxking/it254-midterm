4a8bcf82b415459b8f2c6c1fe9e209e8d176b553
David King <dxking16@gmail.com>
2020-05-27 14:46:01 -0700

    added custom html

--- kickstart.sh
+++ kickstart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# update/upgrade and install docker
+# update/upgrade and install docker/git
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-sudo apt install docker.io -y
+sudo apt install docker.io git -y
 
 # configure ufw
 sudo ufw allow ssh
@@ -10,5 +10,7 @@ sudo ufw allow http
 # force is required to bypass prompt
 sudo ufw --force enable
 
+git clone https://github.com/dxking/it254-midterm
+
 # launch an nginx container with port 80 exposed
-sudo docker run --name midterm-nginx -p 80:80 -d nginx
+sudo docker run --name midterm-nginx -v it254-midterm/html:/usr/share/nginx/html:ro -p 80:80 -d nginx
