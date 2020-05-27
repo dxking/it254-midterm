1b28560558be2893c72d3fceb6f6884d29f0bd1d
David King <dxking16@gmail.com>
2020-05-27 15:06:40 -0700

    minor changes

--- kickstart.sh
+++ kickstart.sh
@@ -10,7 +10,8 @@ sudo ufw allow http
 # force is required to bypass prompt
 sudo ufw --force enable
 
-git clone https://github.com/dxking/it254-midterm
+# this repo has a custom index.html i'd like to attach to my container
+git clone https://github.com/dxking/it254-midterm /tmp/it254-midterm
 
-# launch an nginx container with port 80 exposed
-sudo docker run --name midterm-nginx -v it254-midterm/html:/usr/share/nginx/html:ro -p 80:80 -d nginx
+# launch an nginx container with port 80 exposed and the custom index.html in place
+sudo docker run --name midterm-nginx -v /tmp/it254-midterm/html:/usr/share/nginx/html:ro -p 80:80 -d nginx
--- midterm.sh
+++ midterm.sh
@@ -3,24 +3,24 @@
 # grab my local public IP
 lPubIp="`curl -s https://checkip.amazonaws.com`/32"
 sshKey='aws'
-amiID='068663a3c619dd892'
+amiID='068663a3c619dd892' #ubuntu 20.04
 iCount='1'
 iType='t2.micro'
-iVpcId='vpc-64b8b41e'
+iVpcId='vpc-64b8b41e' # this is my default aws vpc
 
 # create security group and find Id with jq
 iSgName="midterm-sg-`head /dev/urandom | tr -dc a-z0-9 | head -c 3`"
 iSg=`aws ec2 create-security-group --group-name $iSgName --description 'midterm security group' --vpc-id $iVpcId`
 iSgId=`echo $iSg | jq --raw-output '.GroupId'`
-# add custom rules to security group
+# add custom rules to security group to allow ssh/http
 aws ec2 authorize-security-group-ingress --group-id $iSgId --protocol tcp --port 22 --cidr $lPubIp
 aws ec2 authorize-security-group-ingress --group-id $iSgId --protocol tcp --port 80 --cidr 0.0.0.0/0
 
-# launch ec2 instance
-aws ec2 run-instances \
+# launch ec2 instance and send kickstart.sh to run once instance is up
+instance=`aws ec2 run-instances \
 --image-id ami-$amiID \
 --count $iCount \
 --instance-type $iType \
 --key-name $sshKey \
 --security-group-ids $iSgId \
---user-data file://kickstart.sh
+--user-data file://kickstart.sh`
