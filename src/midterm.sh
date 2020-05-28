75c838cbd4e17214942a76baaf2ec2aef3468eba
David King <dxking16@gmail.com>
2020-05-27 18:23:35 -0700

    move vars around

--- src/midterm.sh
+++ src/midterm.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 rand=`head /dev/urandom | tr -dc a-z0-9 | head -c 5`
-
-# generate a new aws keypair and ensure correct key permissions
-sshKey="aws-$rand"
-aws ec2 create-key-pair --key-name $sshKey --query 'KeyMaterial' --output text > ~/.ssh/$sshKey.pem
-chmod 400 ~/.ssh/$sshKey.pem
-
-# grab my local public IP for hardening instance SSH access
-lPubIp="`curl -s https://checkip.amazonaws.com`/32"
 amiId='ami-068663a3c619dd892' # ubuntu 20.04
+iSgName="midterm-sg-$rand"
+sshKey="aws-$rand"
 iCount='1'
 iType='t2.micro'
+# grab my local public IP in cidr format for hardening instance SSH access
+lPubIp="`curl -s https://checkip.amazonaws.com`/32"
 # grab default vpc Id
 iVpcId=`aws ec2 describe-vpcs --filters Name=isDefault,Values=true | jq --raw-output '.Vpcs[0].VpcId'`
-
 # create security group and find Id with jq
-iSgName="midterm-sg-$rand"
 iSg=`aws ec2 create-security-group --group-name $iSgName --description 'midterm security group' --vpc-id $iVpcId`
 iSgId=`echo $iSg | jq --raw-output '.GroupId'`
 
 # add custom rules to security group to allow ssh/http
 aws ec2 authorize-security-group-ingress --group-id $iSgId --protocol tcp --port 22 --cidr $lPubIp
 aws ec2 authorize-security-group-ingress --group-id $iSgId --protocol tcp --port 80 --cidr 0.0.0.0/0
+
+# generate a new aws keypair and ensure correct key permissions
+aws ec2 create-key-pair --key-name $sshKey --query 'KeyMaterial' --output text > ~/.ssh/$sshKey.pem
+chmod 400 ~/.ssh/$sshKey.pem
 
 # launch ec2 instance and send kickstart.sh to run once instance is up
 instance=`aws ec2 run-instances \
